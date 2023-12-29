758da58e57a82e25579e7dc1cf31e737d521f14f
Oussema Sahbeni <104161749+Oussemasahbeni@users.noreply.github.com>
2023-12-29 17:39:29 +0100

    finished

--- src/app/service/github-api.service.ts
+++ src/app/service/github-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments';
+import { environment } from '../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    token: "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"
+};
