93abbb758788ff788e0b3dbc02b09ce0eb69453b
Oussema Sahbeni <104161749+Oussemasahbeni@users.noreply.github.com>
2023-12-29 18:03:43 +0100

    updated

--- src/app/service/github-api.service.ts
+++ src/app/service/github-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+
 
 
 @Injectable({
@@ -10,14 +10,11 @@ export class GithubApiService {
 
   constructor(private http: HttpClient) { }
 
-  private headers = new HttpHeaders({
-    'Authorization': `token ${environment.token}`
-  })
 
 
 
   getInfo() {
-    return this.http.get('https://api.github.com/users/Oussemasahbeni', { headers: this.headers });
+    return this.http.get('https://api.github.com/users/Oussemasahbeni');
   }
 
 }
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    token: "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"
-};
