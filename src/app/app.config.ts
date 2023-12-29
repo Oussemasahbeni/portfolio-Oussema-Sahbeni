15fb73eb60752644b745cd804bae812768d3e161
Oussema Sahbeni <104161749+Oussemasahbeni@users.noreply.github.com>
2023-12-29 21:08:03 +0100

    fixed bundle size

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
+  providers: [provideHttpClient()]
 };
