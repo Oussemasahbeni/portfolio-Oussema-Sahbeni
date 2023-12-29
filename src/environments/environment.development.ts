bc5b42b188a4a765c73260c7b570890c6222c355
Oussema Sahbeni <104161749+Oussemasahbeni@users.noreply.github.com>
2023-12-29 17:46:44 +0100

    small update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
+    production: false,
     token: "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"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
+    production: true,
     token: "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"
 };
