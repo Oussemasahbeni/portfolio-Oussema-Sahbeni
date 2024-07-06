471e1bad07be9c9859dc8ec383f6b844c39a52f4
Oussema Sahbeni <104161749+Oussemasahbeni@users.noreply.github.com>
2024-07-06 10:08:28 +0100

    add skills logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,15 @@ module.exports = {
         sm: "624px",
         md: "768px",
       },
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
     },
     variants: {
       extend: {
